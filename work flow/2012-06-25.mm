cff9d5c403ab10e618fd91400baacb9ee196d968
zinsser <zoezinsser@gmail.com>
2012-07-03 14:18:48 +0800

    当前需要完成的工作

---
+++ work flow/2012-06-25.mm
@@ -0,0 +1,14 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1340611642796" ID="ID_570081502" MODIFIED="1340611822531" STYLE="bubble" TEXT="2012-06-25">
+<node CREATED="1340611695812" HGAP="16" ID="ID_570033012" MODIFIED="1340611950562" POSITION="right" TEXT="KingCAI" VSHIFT="-7">
+<node CREATED="1340611843015" ID="ID_715979549" MODIFIED="1340611934015" TEXT="login&#x4f18;&#x5316;&#xff1a;&#xa;1&#xff0c;&#x663e;&#x793a;&#x57fa;&#x672c;&#x4fe1;&#x606f;&#x3001;&#x5b66;&#x4e60;&#x4fe1;&#x606f;&#xa;2&#xff0c;&#x589e;&#x52a0;&#x5934;&#x50cf;&#xa;===&gt;&#x6570;&#x636e;&#x5e93;&#x7ba1;&#x7406;&#x5b66;&#x751f;&#x4fe1;&#x606f;"/>
+<node CREATED="1340611950578" ID="ID_836979566" MODIFIED="1340612341343" TEXT="Paper&#x4f18;&#x5316;&#xff1a;&#xa;1&#xff0c;&#x5c06;&#x8001;&#x5e08;&#x4e0b;&#x53d1;&#x7684;&#x6bcf;&#x4e00;&#x9053;&#x8bd5;&#x9898;&#x4fdd;&#x5b58;&#x5230;&#x672c;&#x5730;&#x8bd5;&#x9898;&#x5e93;&#xff0c;&#x4ee5;&#x6784;&#x5efa;&#x672c;&#x5730;&#x8bd5;&#x9898;&#x5e93;&#xa;  ----&gt;&#x672c;&#x5730;&#x8bd5;&#x9898;&#x5e93;&#x9700;&#x8981;&#x6709;&#x96be;&#x5ea6;&#x7b49;&#x7ea7;&#x7cfb;&#x6570;&#xff08;&#x6839;&#x636e;&#x51fa;&#x9519;&#x6b21;&#x6570;&#x6765;&#x7b97;&#xff09;&#xa;2&#xff0c;autosave &#x95ee;&#x9898;&#x89e3;&#x51b3;&#xff0c;&#x51cf;&#x5c11;&#x7535;&#x6c60;&#x65ad;&#x7535;&#x5e26;&#x6765;&#x7684;&#x5f71;&#x54cd;&#xa;3&#xff0c;&#x589e;&#x52a0;&#x8bd5;&#x9898;&#x5bfc;&#x5165;&#x529f;&#x80fd;&#xff1a;&#xa;  ----&gt;&#x4ee5;&#x6392;&#x9664;&#x8003;&#x8bd5;&#x65f6;&#xff0c;&#x7531;&#x4e8e;&#x7f51;&#x7edc;&#x95ee;&#x9898;&#x4e0d;&#x80fd;&#x89e3;&#x51b3;&#xff0c;&#xa;              &#x8bd5;&#x5377;&#x65e0;&#x6cd5;&#x6b63;&#x5e38;&#x53d1;&#x5230;&#x5b66;&#x751f;&#x673a;&#xff0c;&#x5bfc;&#x81f4;&#x65e0;&#x6cd5;&#x6b63;&#x5e38;&#x8003;&#x8bd5;&#x95ee;&#x9898;&#xa;4&#xff0c;&#x56fe;&#x7247;&#x63a5;&#x6536;&#x4f18;&#x5316;&#x3001;&#x663e;&#x793a;&#x7684;&#x5e03;&#x5c40;&#x4f18;&#x5316;"/>
+</node>
+<node CREATED="1340611705796" ID="ID_98931509" MODIFIED="1340611792375" POSITION="left" TEXT="WordStudy"/>
+<node CREATED="1340611714546" ID="ID_479881598" MODIFIED="1340611792375" POSITION="right" TEXT="RPC/QMI/RIL"/>
+<node CREATED="1340611723000" HGAP="37" ID="ID_1073813608" MODIFIED="1340611828312" POSITION="left" TEXT="&#x4e13;&#x5229;" VSHIFT="2"/>
+<node CREATED="1340611741406" ID="ID_1598289831" MODIFIED="1340611792375" POSITION="right" TEXT="&#x9879;&#x76ee;&#x95ee;&#x9898;"/>
+<node CREATED="1340611810718" ID="ID_44337146" MODIFIED="1340611817140" POSITION="left" TEXT="&#x4eba;&#x8138;&#x8bc6;&#x522b;"/>
+</node>
+</map>
