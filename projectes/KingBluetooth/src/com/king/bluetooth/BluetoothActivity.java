2e0cf7d364d983ab039168e3935f0a80174b3fa4
zinsser <zoezinsser@gmail.com>
2012-07-20 16:26:16 +0800

    1，加入bluetooth服务 2，将kingbluetoothactivity更名为bluetoothactivity

---
+++ projectes/KingBluetooth/src/com/king/bluetooth/BluetoothActivity.java
@@ -0,0 +1,24 @@
+package com.king.bluetooth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BluetoothActivity extends Activity {
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";    
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
